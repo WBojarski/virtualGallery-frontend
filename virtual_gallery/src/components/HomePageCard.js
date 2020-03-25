402a69af89e47a8b8483ed55adb9fc89612ae657
pixulainen <george.apetrei@gmail.com>
2020-03-25 18:18:30 +0000

    add

--- virtual_gallery/src/components/HomePageCard.js
+++ virtual_gallery/src/components/HomePageCard.js
@@ -4,6 +4,26 @@ import { NewsHeaderCard } from 'react-ui-cards';
 import styles from 'react-ui-cards';
 
 export default class HomePageCard extends Component {
+	postArtPiece(event, artPiece) {
+		event.preventDefault();
+		this.addArtToCollection(artPiece);
+	}
+	addArtToCollection(artPiece, collectionid = 1) {
+		let object = {
+			art_piece_id: artPiece.id,
+			collection_id: collectionid
+		};
+		fetch('http://localhost:3001/art_piece_collections', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(object)
+		})
+			.then((resp) => resp.json())
+			.then((obj) => console.log(obj));
+	}
 	render() {
 		let { artPiece } = this.props;
 		return (
@@ -14,9 +34,12 @@ export default class HomePageCard extends Component {
 					title={artPiece.title}
 					author={artPiece.artistDisplayName}
 					date={
-						<Button circular icon="save outline" color="grey">
-							{/* <Icon name="save outline" size="big" color="white" /> */}
-						</Button>
+						<Button
+							onClick={(event) => this.postArtPiece(event, artPiece)}
+							circular
+							icon="save outline"
+							color="grey"
+						/>
 					}
 					tags={[ artPiece.classification, () => <i>{artPiece.culture}</i> ]}
 				/>
--- virtual_gallery/src/containers/HomePageCardsContainer.js
+++ virtual_gallery/src/containers/HomePageCardsContainer.js
@@ -1,62 +1,48 @@
-import React, { Component } from 'react'
-import HomePageCard from '../components/HomePageCard'
+import React, { Component } from 'react';
+import HomePageCard from '../components/HomePageCard';
 
-import { Container } from 'react-bootstrap'
-import { Row } from 'react-bootstrap'
-import { Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
-import styles from '../custom.scss'
-
-
+import styles from '../custom.scss';
 
 export default class HomePageCardsContainer extends Component {
+	state = {
+		artPieces: []
+	};
+	selectRandomArtPieces(art) {
+		const withPhotos = art.filter((piece) => piece.primaryImageSmall.length > 1 && piece.culture.length > 1);
+		const shuffled = withPhotos.sort(() => {
+			return 0.5 - Math.random();
+		});
+		const selected = shuffled.slice(0, 100);
+		return this.renderArtPieceCards(selected);
+	}
 
-    state = {
-        artPieces: []
-    }
-    selectRandomArtPieces(art) {
-        const withPhotos = art.filter(piece => piece.primaryImageSmall.length > 1 && piece.culture.length > 1)
-        const shuffled = withPhotos.sort(() => {
-            return 0.5 - Math.random()
-        })
-        const selected = shuffled.slice(0, 100)
-        return this.renderArtPieceCards(selected)
+	componentDidMount() {
+		const artPiecesURL = 'http://localhost:3001/art_pieces';
+		fetch(artPiecesURL).then((resp) => resp.json()).then((artPieces) =>
+			this.setState({
+				artPieces: artPieces
+			})
+		);
+	}
 
-    }
+	renderArtPieceCards = (artPieces) => {
+		if (artPieces && artPieces.length > 0) {
+			return artPieces.map((artPiece) => {
+				console.log(artPiece);
+				return <HomePageCard artPiece={artPiece} key={artPiece.id} />;
+			});
+		}
+	};
 
-    componentDidMount() {
-        const artPiecesURL = "http://localhost:3001/art_pieces"
-        fetch(artPiecesURL)
-            .then(resp => resp.json())
-            .then(artPieces => this.setState({
-                artPieces: artPieces
-            }))
-
-    }
-
-    renderArtPieceCards = (artPieces) => {
-        if (artPieces && artPieces.length > 0) {
-            return artPieces.map(artPiece => {
-                console.log(artPiece)
-                return <HomePageCard artPiece={artPiece} />
-
-            })
-        }
-
-    }
-
-
-    render() {
-        return (
-            <div>
-                <Container className="card-container">
-
-                    {this.selectRandomArtPieces(this.state.artPieces)}
-                </Container>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<Container className="card-container">{this.selectRandomArtPieces(this.state.artPieces)}</Container>
+			</div>
+		);
+	}
 }
-
-
-
